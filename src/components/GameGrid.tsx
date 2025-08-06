fbebe958812ddf720eee96ff63a3cee3693c912e
Luis Mazariego <mazariego2011@outlook.com>
2025-08-06 00:39:32 -0600

    building game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,22 @@
-import { Text } from "@chakra-ui/react";
-import useGames from '../hooks/useGames';
+import { Text, SimpleGrid } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  
-  const {error, games} = useGames();
+  const { error, games } = useGames();
 
   return (
     <>
       {error ?? <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding={10}
+        spacing={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
